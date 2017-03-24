bdfd0bac8944b01957975da67d70271069e2f78a
Angas.Liu <yueguang0514@126.com>
2017-03-24 16:29:55 +0000

    Add joke feature

--- Source/GrabDate/Program.cs
+++ Source/GrabDate/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,28 @@ namespace GrabDate
     {
         static void Main(string[] args)
         {
+            WebClient webClient = new WebClient();
+            webClient.Headers.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            var downloadString = webClient.DownloadString("http://www.qiushibaike.com/");
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(downloadString);
+
+            var contents = htmlDocument.DocumentNode
+                    .Descendants("div")
+                    .Where(d => d.Attributes.Contains("class")
+                       && d.Attributes["class"].Value.Contains("content-text"));
+
+            var items = htmlDocument.DocumentNode
+                .SelectNodes("//div[@class='content-text']");
+
+
+            //Console.OutputEncoding = Encoding.GetEncoding(936);
+            var index = 1;
+            foreach (var item in items)
+            {
+                Console.WriteLine(string.Format("{0}. {1}", index, item.InnerText.Trim()));
+                index++;
+            }
         }
     }
 }
